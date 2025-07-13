d7fdf66d875959b04b86c91642ed68446200c7e8
8340693404 <mithunmishra024@gmail.com>
2025-07-13 19:30:37 +0530

    2nd commit

---
+++ src/test/java/jenkins_Practice/Gitbash_Prac.java
@@ -0,0 +1,11 @@
+package jenkins_Practice;
+
+public class Gitbash_Prac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
