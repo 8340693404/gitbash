f76c919626fb789e3c6a530c0767df62ee4d02d9
8340693404 <mithunmishra024@gmail.com>
2025-07-13 19:06:35 +0530

    1st commit

---
+++ src/test/java/jenkins_Practice/Jenkins_prac.java
@@ -0,0 +1,11 @@
+package jenkins_Practice;
+
+public class Jenkins_prac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hiiii");
+
+	}
+
+}
